4cd8cb014c94683a695426178d1a9e73f495b521
Ryan Abernathey <rpa@ldeo.columbia.edu>
2016-05-05 12:13:13 -0400

    got more stuff working

--- flask_app/app.py
+++ flask_app/app.py
@@ -10,29 +10,11 @@ mongo = PyMongo(app)
 def index():
     return "Hello, World!"
 
-# some test data for leaflet to plot
-eddy_data ={
-    'type': 'FeatureCollection',
-    'features':[
-        {'type': 'Feature',
-         'properties': {'name': 'start', 'date': '1992-01-01'},
-         'geometry': {'type': 'Point',
-                    'coordinates': [200,35]}},
-        {'type': 'Feature',
-         'properties': {'name': 'end', 'date': '1992-01-21'},
-         'geometry': {'type': 'Point',
-                    'coordinates': [190,33]}},
-        {'type': 'Feature',
-         'properties': {'name': 'trajectory', 'date': '1992-01-21'},
-         'geometry': {'type': 'LineString',
-                    'coordinates': [[200,35],[190,33]]}},
-    ]
-}
-
-@app.route('/eddy')
-def get_eddy():
-    """A single test eddy."""
-    return jsonify(eddy_data)
+@app.route('/eddy/<eddy_id>')
+def get_eddy(eddy_id):
+    """Full data for eddy."""
+    eddy = mongo.db.rcs_eddies.find_one({'_id': eddy_id})
+    return jsonify(eddy)
 
 @app.route('/eddies')
 def get_eddies(full_data=False):
