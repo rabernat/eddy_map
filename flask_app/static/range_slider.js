b45ab45ac567aa49159c3533d265329bb153655f
Ci Zhang <zhangci@yahoo.com>
2016-06-14 15:19:19 -0400

    revised jQRangeSlider

--- flask_app/static/range_slider.js
+++ flask_app/static/range_slider.js
@@ -2,22 +2,32 @@
 
 // Date
 $("#dateSlider").dateRangeSlider({arrows:false},
-                                 {bounds:{min: new Date(1992, 9, 14), max: new Date(2012, 3, 4)}},
-                                 {defaultValues:{min: new Date(1992, 9, 14), max: new Date(2012, 3, 4)}},
-                                 {valueLabels:"change", delayOut: 2000});
+                                 {bounds:{min: new Date(1992, 9, 4), max: new Date(2012, 3, 4)}},
+                                 {defaultValues:{min: new Date(1992, 9, 4), max: new Date(2012, 3, 4)}},
+                                 {valueLabels:"change", delayOut: 4000, durationIn: 1000, durationOut: 1000},
+                                 {step:{days:1}},
+                                 {wheelMode: "scroll"/*, wheelSpeed: 1*/});
 
 // Latitude
 $("#slider_lat").rangeSlider({arrows:false},
                              {bounds:{min: -90, max: 90}},
                              {defaultValues:{min: -90, max: 90}},
-                             {valueLabels:"change", delayOut: 2000});
+                             {valueLabels:"change", delayOut: 4000, durationIn: 1000, durationOut: 1000},
+                             {step: 1},
+                             {wheelMode: "scroll"/*, wheelSpeed: 1*/});
+
 // Longitude
 $("#slider_lon").rangeSlider({arrows:false},
                              {bounds:{min: 0, max: 360}},
                              {defaultValues:{min: 0, max: 360}},
-                             {valueLabels:"change", delayOut: 2000});
+                             {valueLabels:"change", delayOut: 4000, durationIn: 1000, durationOut: 1000},
+                             {step: 1},
+                             {wheelMode: "scroll"/*, wheelSpeed: 1*/});
+
 // Duration
 $("#slider_dur").rangeSlider({arrows:false},
                              {bounds:{min: 3, max: 167}},
                              {defaultValues:{min: 3, max: 167}},
-                             {valueLabels:"change", delayOut: 2000});
+                             {valueLabels:"change", delayOut: 4000, durationIn: 1000, durationOut: 1000},
+                             {step: 1},
+                             {wheelMode: "scroll"/*, wheelSpeed: 1*/});
