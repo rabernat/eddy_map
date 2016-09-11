903cc57cb597fb2c4834bdf7992b6b627c55a781
Ci Zhang <zhangci@yahoo.com>
2016-09-11 12:49:45 -0400

    before results

--- flask_app/static/my_map.js
+++ flask_app/static/my_map.js
@@ -15,7 +15,9 @@ var tileLayer03 = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.
 
 // –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– map ––––– //
 var myMap = L.map("mapid", {
-    fullscreenControl: true,
+    fullscreenControl: {
+        pseudoFullscreen: false
+    },
     center: [0, 180],
     zoom: 1,
     layers: tileLayer01,
